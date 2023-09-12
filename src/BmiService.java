f5540052a2b0040179dc6e6e73ea4dd31638e445
Igor Reus <igorsreus@gmail.com>
2023-09-12 16:06:35 +0300

    second

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int weight, int height) {
-        int bmi = (weight * 10000/ (height * height));
-    return  bmi;
+        int bmi = (weight * 10000 / (height * height));
+        return bmi;
     }
 }
